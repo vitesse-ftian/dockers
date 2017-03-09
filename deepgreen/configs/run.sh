196b0f3e14b4b8e0be305b72932c68993c7d70f0
Feng Tian <fengttt@gmail.com>
2017-03-09 11:12:37 -0800

    Minor.

--- deepgreen/configs/run.sh
+++ deepgreen/configs/run.sh
@@ -32,39 +32,39 @@ if [ -f ./persist/db/hostfile ]; then
     #
 
     # Still need to exchange keys again, because .ssh is not persisted.
-	gpssh-exkeys -f ./persist/db/hostfile
+    gpssh-exkeys -f ./persist/db/hostfile
 
-	echo "Start deepgreen database ..."
-	gpstart -a
-	echo "Start xdrive ..."
-	xdrctl start ~/xdrive.toml
+    echo "Start deepgreen database ..."
+    gpstart -a
+    echo "Start xdrive ..."
+    xdrctl start ~/xdrive.toml
 else 
-	sudo chown deepgreen ~/cluster.conf
-	chmod 755 ~/cluster.conf
-	sudo chown deepgreen /home/deepgreen/persist
+    sudo chown deepgreen ~/cluster.conf
+    chmod 755 ~/cluster.conf
+    sudo chown deepgreen /home/deepgreen/persist
 
     # Hack is not necessary.
     # hostname > /home/deepgreen/persist/hostname_at_gpinit
 
-	echo "Init deepgreen database ..."
-	mkdir -p /home/deepgreen/persist/db
-	cd /home/deepgreen/persist/db 
-	echo localhost > ./hostfile
-	gpssh-exkeys -f ./hostfile
-	mkdir -p data{0,1,2,3,4}
-	gpinitsystem -a -c ~/cluster.conf --lc-collate=C
-	echo "host all all 0.0.0.0/0 md5" >> $MASTER_DATA_DIRECTORY/pg_hba.conf
-	createdb deepgreen
+    echo "Init deepgreen database ..."
+    mkdir -p /home/deepgreen/persist/db
+    cd /home/deepgreen/persist/db 
+    echo localhost > ./hostfile
+    gpssh-exkeys -f ./hostfile
+    mkdir -p data{0,1,2,3,4}
+    gpinitsystem -a -c ~/cluster.conf --lc-collate=C
+    echo "host all all 0.0.0.0/0 md5" >> $MASTER_DATA_DIRECTORY/pg_hba.conf
+    createdb deepgreen
     dg setup -all template1
     dg setup -all deepgreen
-	psql -d template1 -c "alter user deepgreen password 'deepGr33n'"
-	gpstop -a -M fast
-	gpstart -a
+    psql -d template1 -c "alter user deepgreen password 'deepGr33n'"
+    gpstop -a -M fast
+    gpstart -a
 
-	echo "Init XDrive ..."
-	xdrctl deploy ~/xdrive.toml
-	mkdir -p /home/deepgreen/persist/xdrive/data
-	xdrctl start ~/xdrive.toml 
+    echo "Init XDrive ..."
+    xdrctl deploy ~/xdrive.toml
+    mkdir -p /home/deepgreen/persist/xdrive/data
+    xdrctl start ~/xdrive.toml 
 fi
 
 echo "Deepgeen Started.   Enjoy!" 
